aa0b31771e209ea549d8782ed1918fcb1f804a69
Jackaria72 <jack.goodwill@hotmail.co.uk>
2025-03-19 01:03:39 +0000

    Fix: correct base url

--- app/src/main/java/com/artful/curatolist/network/RetrofitInstance.kt
+++ app/src/main/java/com/artful/curatolist/network/RetrofitInstance.kt
@@ -6,7 +6,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 object RetrofitInstance {
     val api : ApiService by lazy {
         Retrofit.Builder()
-            .baseUrl("http://10.0.2.2:8080/api/v1/")
+            .baseUrl("http://10.0.2.2:8080/curatolist/api/v1/")
             .addConverterFactory(GsonConverterFactory.create())
             .build()
             .create(ApiService::class.java)
