748fc5b9c5abba289bc9eb7183bc3a296ff8af48
Jackaria72 <jack.goodwill@hotmail.co.uk>
2025-03-19 15:46:04 +0000

    Feat: implement pagination for inital call in Search Screen

--- app/src/main/java/com/artful/curatolist/ui/view/SearchScreen.kt
+++ app/src/main/java/com/artful/curatolist/ui/view/SearchScreen.kt
@@ -1,42 +1,70 @@
 package com.artful.curatolist.ui.view
 
+import android.util.Log
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.artful.curatolist.model.Artwork
 import com.artful.curatolist.ui.cards.ArtworkItem
+import com.artful.curatolist.ui.components.PaginationControls
 import com.artful.curatolist.viewmodel.ArtworkViewModel
 
 
 @Composable
 fun SearchScreen(viewModel: ArtworkViewModel = viewModel()) {
-    val artwork = viewModel.art.value
+    val allArtwork = viewModel.art.value
+    val artwork = viewModel.paginatedArtwork.collectAsStateWithLifecycle().value
+    val currentAppPage = viewModel.currentAppPage.value
+    val isLoading = viewModel.isLoading.value
+
 
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .padding(16.dp)
+
     ) {
-        if (artwork.isEmpty()) {
+        if (isLoading) {
             CircularProgressIndicator(
-                modifier = Modifier.fillMaxSize()
+                modifier = Modifier
+                    .size(100.dp)
+                    .align(alignment = Alignment.CenterHorizontally),
             )
         } else {
             LazyColumn(
-                modifier = Modifier.fillMaxSize()
+                modifier = Modifier
+                    .weight(1f)
+                    .fillMaxWidth()
             ) {
                 items(items = artwork) {
-                    item -> ArtworkItem(item)
+                    item: Artwork -> ArtworkItem(item)
                 }
                 }
             }
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            PaginationControls(
+                onPreviousClick = { viewModel.previousPage() },
+                onNextClick = { viewModel.nextPage() },
+                previousEnabled = currentAppPage > 0,
+                nextEnabled = (currentAppPage+1)*20 < allArtwork.size
+            )
+        }
         }
     }
