eb53e7b883bba12e4175807fb05b4c84af2004f9
Jack <jack.goodwill@hotmail.co.uk>
2025-03-19 01:06:51 +0000

    Fix/network (#3)
    * Fix: add clear text trafic to manifest
    
    * Fix: correct base url
    
    * Fix: add response Page model
    
    * Fix: correct API response type implementation

---
+++ app/src/main/java/com/artful/curatolist/model/ResponsePage.kt
@@ -0,0 +1,15 @@
+package com.artful.curatolist.model
+
+import androidx.compose.foundation.pager.PageInfo
+
+data class ResponsePage(
+    val pageInfo: PageInfo,
+    val artwork: List<Artwork>
+) {
+    data class PageInfo(
+        val chicagoTotal: Int,
+        val chicagoPageTotal: Int,
+        val harvardTotal: Int,
+        val harvardPageTotal: Int
+    )
+}
--- app/src/main/java/com/artful/curatolist/network/ApiService.kt
+++ app/src/main/java/com/artful/curatolist/network/ApiService.kt
@@ -1,10 +1,11 @@
 package com.artful.curatolist.network
 
 import com.artful.curatolist.model.Artwork
+import com.artful.curatolist.model.ResponsePage
 import retrofit2.http.GET
 
 interface ApiService
 {
     @GET("art")
-    suspend fun getArt(): List<Artwork>
+    suspend fun getArt(): ResponsePage
 }
