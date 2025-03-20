4833cd3ac06f9ede89ea81b45e4882373d8fa1b8
Jack <jack.goodwill@hotmail.co.uk>
2025-03-20 21:03:20 +0000

    Feature/images (#10)
    * Feat: add coil dependencies
    
    * Feat: add async image to details screen
    
    * Feat: add async image to artwork card
    
    * Feat: add error and placeholder drawables

--- app/src/main/java/com/artful/curatolist/ui/cards/ArtworkItem.kt
+++ app/src/main/java/com/artful/curatolist/ui/cards/ArtworkItem.kt
@@ -1,22 +1,39 @@
 package com.artful.curatolist.ui.cards
 
+
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import coil3.compose.AsyncImage
+import coil3.compose.rememberAsyncImagePainter
+import coil3.request.ImageRequest
+import coil3.request.error
+import coil3.request.placeholder
+import com.artful.curatolist.R
 import com.artful.curatolist.model.Artwork
 
 @Composable
@@ -29,20 +46,43 @@ fun ArtworkItem(artwork: Artwork, onClick: () -> Unit) {
     elevation = CardDefaults.cardElevation(defaultElevation = 4.dp),
     shape = RoundedCornerShape(8.dp)
     ) {
-
-            Column(
-                verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.Start,
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(10.dp),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            AsyncImage(
+                model = ImageRequest.Builder(LocalContext.current)
+                    .data(artwork.imageUrl)
+                    .error(R.drawable.ic_error)
+                    .placeholder(R.drawable.ic_placeholder)
+                    .build(),
+                contentDescription = "Artwork Image",
                 modifier = Modifier
-                    .padding(4.dp)
-                    .fillMaxHeight()
+                    .size(80.dp)
+                    .clip(RoundedCornerShape(8.dp)),
+            )
+            Spacer(modifier = Modifier
+                .width(16.dp))
+            Column(
+                modifier = Modifier,
+                horizontalAlignment = Alignment.End
             ) {
-                Text(text = artwork.title)
-                Text(text = artwork.artist)
-                Text(text = artwork.classification)
+                Text(
+                    text = artwork.title,
+                    style = MaterialTheme.typography.titleMedium
+                )
+                Text(
+                    text = artwork.artist,
+                    style = MaterialTheme.typography.bodyMedium
+                )
+                Text(
+                    text = artwork.classification,
+                    style = MaterialTheme.typography.bodySmall
+                )
             }
-
-
+        }
     }
 }
 @Preview(showBackground = true)
@@ -59,8 +99,9 @@ fun PreviewArtworkItem() {
         classification = "painting",
         culturalOrigin = "test",
         dimensions = "test",
-        imageUrl = "test",
+        imageUrl = "https://picsum.photos/200/300",
         source = "Harvard"
     )
+    ArtworkItem(sampleArtwork, {  })
 
 }
