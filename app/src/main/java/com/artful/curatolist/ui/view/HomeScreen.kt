3514485f7118829bc989b94df4be98c33d073863
Jackaria72 <jack.goodwill@hotmail.co.uk>
2025-03-20 02:19:26 +0000

    Feat: implement search bar in Home screen

--- app/src/main/java/com/artful/curatolist/ui/view/HomeScreen.kt
+++ app/src/main/java/com/artful/curatolist/ui/view/HomeScreen.kt
@@ -1,9 +1,32 @@
 package com.artful.curatolist.ui.view
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+import com.artful.curatolist.ui.components.AppSearchBar
+import com.artful.curatolist.ui.navigation.NavDestination
+import com.artful.curatolist.viewmodel.ArtworkViewModel
 
 @Composable
-fun HomeScreen() {
-    Text("Home")
+fun HomeScreen(navController: NavController, viewModel: ArtworkViewModel) {
+    var query by viewModel.q
+
+    Column(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        AppSearchBar(
+            query = query,
+            onQueryChange = { query = it },
+            onSearch = { navController.navigate(NavDestination.Search.createRoute(query)) },
+            false,
+            {  }
+        )
+    }
 }
