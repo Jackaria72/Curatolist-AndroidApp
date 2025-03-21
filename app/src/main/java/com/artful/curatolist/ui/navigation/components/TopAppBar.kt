87711856fae1b46b9b1bff43cef574cf4c2b4d20
Jackaria72 <jack.goodwill@hotmail.co.uk>
2025-03-21 00:19:23 +0000

    Feat: Create Top App bar button to access extra screens

--- app/src/main/java/com/artful/curatolist/ui/navigation/components/TopAppBar.kt
+++ app/src/main/java/com/artful/curatolist/ui/navigation/components/TopAppBar.kt
@@ -2,12 +2,17 @@ package com.artful.curatolist.ui.navigation.components
 
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.navigation.NavController
 import androidx.navigation.compose.currentBackStackEntryAsState
 
@@ -16,17 +21,39 @@ import androidx.navigation.compose.currentBackStackEntryAsState
 @Composable
 fun TopBar(navController: NavController) {
 
-    val currentDestination =navController.currentBackStackEntryAsState().value?.destination?.route
+    var expanded = remember { mutableStateOf(false) }
+    val currentDestination = navController.currentBackStackEntryAsState().value?.destination?.route
 
     TopAppBar(
-        title = {Text("Curatolist") },
+        title = { Text("Curatolist") },
         navigationIcon = {
-            if (currentDestination == "details_screen") {
+            if (currentDestination == "details_screen" || currentDestination == "acknowledgement_screen") {
                 IconButton(onClick = {
-                    navController.previousBackStackEntry?.savedStateHandle?.set("shouldTriggerSearch",false)
-                    navController.popBackStack() }) {
+                    navController.previousBackStackEntry?.savedStateHandle?.set(
+                        "shouldTriggerSearch",
+                        false
+                    )
+                    navController.popBackStack()
+                }) {
                     Icon(Icons.AutoMirrored.Filled.ArrowBack, contentDescription = "Back")
                 }
+            }
+        },
+        actions = {
+            IconButton(onClick = { expanded.value = true }) {
+                Icon(Icons.Default.MoreVert, contentDescription = "More Options")
+            }
+            DropdownMenu(
+                expanded = expanded.value,
+                onDismissRequest = { expanded.value = false }
+            ) {
+                DropdownMenuItem(
+                    text = { Text("Acknowledgments") },
+                    onClick = {
+                        expanded.value = false
+                        navController.navigate("ackno_screen")
+                    }
+                )
             }
         }
     )
