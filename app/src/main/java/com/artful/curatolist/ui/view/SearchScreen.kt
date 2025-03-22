a075c4f8e197dabea85545566c01176ca9e4c37c
Jackaria72 <jack.goodwill@hotmail.co.uk>
2025-03-22 17:01:28 +0000

    Feat: implement search options in search screen

--- app/src/main/java/com/artful/curatolist/ui/view/SearchScreen.kt
+++ app/src/main/java/com/artful/curatolist/ui/view/SearchScreen.kt
@@ -3,38 +3,35 @@ package com.artful.curatolist.ui.view
 import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.SearchBar
-import androidx.compose.material3.SearchBarDefaults
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import com.artful.curatolist.model.Artwork
+import com.artful.curatolist.model.FilterType
+import com.artful.curatolist.model.FilterType.Companion.filterTypes
+import com.artful.curatolist.model.SearchQueries
+
 import com.artful.curatolist.ui.cards.ArtworkItem
 import com.artful.curatolist.ui.components.AppSearchBar
 import com.artful.curatolist.ui.components.PaginationControls
+import com.artful.curatolist.ui.components.SearchOptions
 import com.artful.curatolist.viewmodel.ArtworkViewModel
 
 
@@ -53,7 +50,26 @@ fun SearchScreen(viewModel: ArtworkViewModel,
     val pageInfo = viewModel.pageInfo.value
 
     var query by viewModel.q
+    val selectedSource = remember { mutableStateOf("Select Source") }
+    val selectedFilter = remember { mutableStateOf<FilterType?>(FilterType.filterTypes[0]) }
+    val selectedFilterValue = remember { mutableStateOf("Select Value") }
+    val selectedSort = remember { mutableStateOf("Select Sort") }
     var isSearchBarActive by remember { mutableStateOf(false) }
+
+    val searchQueries = SearchQueries(
+        search = query,
+        source = selectedSource.value,
+        filterType = selectedFilter.value?.displayName ?: "",
+        filterValue = selectedFilterValue.value,
+        sort = selectedSort.value
+    )
+    val filterValues =
+        if (selectedFilter.value == FilterType.Classification) allArtwork.map { it.classification }.distinct()
+        else if (selectedFilter.value == FilterType.Technique) allArtwork.map { it.technique }.distinct()
+        else if (selectedFilter.value == FilterType.Medium) allArtwork.map { it.medium }.distinct()
+        else emptyList()
+
+    val filterOptions = FilterType.filterTypes.map { it.displayName }
 
     val shouldTriggerSearch = navController.currentBackStackEntry
         ?.savedStateHandle
@@ -62,7 +78,7 @@ fun SearchScreen(viewModel: ArtworkViewModel,
     LaunchedEffect(shouldTriggerSearch) {
         if (shouldTriggerSearch) {
             viewModel.onQueryChange(query)
-            viewModel.getArtList(1, query)
+            viewModel.getArtList(1, searchQueries)
         }
     }
 
@@ -78,9 +94,24 @@ fun SearchScreen(viewModel: ArtworkViewModel,
         AppSearchBar(
             query = query,
             onQueryChange = { viewModel.onQueryChange(it) },
-            onSearch = { viewModel.getArtList(1, query) },
+            onSearch = { viewModel.getArtList(1, searchQueries) },
             isActive = isSearchBarActive,
-            { isSearchBarActive = it } ,
+            { isSearchBarActive = it },
+            content = {
+                SearchOptions(
+                    selectedSource = selectedSource.value,
+                    selectedFilter = selectedFilter.value?.displayName ?: "Select Filter Type",
+                    selectedFilterValue = selectedFilterValue.value,
+                    selectedSort = selectedSort.value,
+                    onSourceSelected = { selectedSource.value = it },
+                    onFilterSelected = {
+                        displayName -> selectedFilter.value = FilterType.filterTypes.first { it.displayName == displayName }
+                        selectedFilterValue.value = "Filter By" },
+                    onFilterValueSelected = { selectedFilterValue.value = it },
+                    onSortSelected = { selectedSort.value = it },
+                    filterValues = filterValues
+                )
+            }
         )
         Box(
             modifier = Modifier
@@ -112,8 +143,8 @@ fun SearchScreen(viewModel: ArtworkViewModel,
             verticalAlignment = Alignment.CenterVertically
         ) {
             PaginationControls(
-                onPreviousClick = { viewModel.previousPage() },
-                onNextClick = { viewModel.nextPage() },
+                onPreviousClick = { viewModel.previousPage(searchQueries) },
+                onNextClick = { viewModel.nextPage(searchQueries) },
                 previousEnabled = currentAppPage > 0 || currentApiPage > 1,
                 nextEnabled = ((currentAppPage+1)*20 < allArtwork.size || currentApiPage < (pageInfo?.combinedPageTotal ?: 0))
             )
