5c1772294f4d138ebce1d6bad572a1d20a37d794
Jackaria72 <jack.goodwill@hotmail.co.uk>
2025-03-23 15:34:02 +0000

    Feat: Create Database class

---
+++ app/src/main/java/com/artful/curatolist/room/database/CuratolistDatabase.kt
@@ -0,0 +1,54 @@
+package com.artful.curatolist.room.database
+
+import android.content.Context
+import android.util.Log
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import androidx.room.RoomDatabase.Callback
+import androidx.sqlite.db.SupportSQLiteDatabase
+import com.artful.curatolist.room.data.ArtListArtWorkCrossRef
+import com.artful.curatolist.room.data.ArtworkItem
+import com.artful.curatolist.room.dao.ArtworkItemDao
+import com.artful.curatolist.room.data.ArtworkList
+import com.artful.curatolist.room.dao.ArtworkListDao
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+
+import kotlinx.coroutines.launch
+
+@Database(
+    entities = [ArtworkList::class, ArtworkItem::class, ArtListArtWorkCrossRef::class],
+    version = 1,
+    exportSchema = false
+)
+abstract class CuratolistDatabase: RoomDatabase() {
+    abstract fun artworkListDao(): ArtworkListDao
+    abstract fun artworkItemDao(): ArtworkItemDao
+
+    private val databaseScope = CoroutineScope(Dispatchers.IO)
+
+    companion object {
+        @Volatile
+        private var INSTANCE: CuratolistDatabase? = null
+        fun getDatabase(context: Context): CuratolistDatabase {
+            return INSTANCE ?: synchronized(this){
+                val instance = Room.databaseBuilder(
+                    context,
+                    CuratolistDatabase::class.java,
+                    "curatolist_database"
+                ).addCallback(object : Callback() {
+                    override fun onCreate(db: SupportSQLiteDatabase) {
+                        super.onCreate(db)
+                        Log.d("Curatolist Database", "Database Created!!!")
+                        db.execSQL("INSERT INTO artwork_list (listName, icon, isDefault) VALUES ('Favorites', 'heart', 1)")
+                    }
+                }).fallbackToDestructiveMigration()
+                    .build()
+                    INSTANCE = instance
+                instance
+            }
+            }
+        }
+
+}
