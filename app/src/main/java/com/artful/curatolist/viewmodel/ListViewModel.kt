b4b603ecbc60f4db95b9b31a87351dcdfd2d6ea7
Jackaria72 <jack.goodwill@hotmail.co.uk>
2025-03-23 19:20:53 +0000

    Feat: Create add to favourites view model method

--- app/src/main/java/com/artful/curatolist/viewmodel/ListViewModel.kt
+++ app/src/main/java/com/artful/curatolist/viewmodel/ListViewModel.kt
@@ -1,34 +1,18 @@
 package com.artful.curatolist.viewmodel
 
-import android.app.Application
-import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.State
-import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
-import androidx.lifecycle.AndroidViewModel
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.asFlow
-import androidx.lifecycle.asLiveData
 import androidx.lifecycle.viewModelScope
 import com.artful.curatolist.model.Artwork
-import com.artful.curatolist.repository.CuratolistRepository
 import com.artful.curatolist.room.Graph
-import com.artful.curatolist.room.data.ArtworkItem
-import com.artful.curatolist.room.dao.ArtworkItemDao
+import com.artful.curatolist.room.data.ArtListArtWorkCrossRef
 import com.artful.curatolist.room.data.ArtworkList
-import com.artful.curatolist.room.dao.ArtworkListDao
 import com.artful.curatolist.room.data.ArtworkListWithArt
-import com.artful.curatolist.room.database.CuratolistDatabase
 import com.artful.curatolist.room.repository.RoomRepository
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.SharingStarted
-import kotlinx.coroutines.flow.StateFlow
+import com.artful.curatolist.viewmodel.util.toArtworkItem
 import kotlinx.coroutines.flow.collectLatest
-import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 
 class ListViewModel(private val repository: RoomRepository = Graph.repository) : ViewModel() {
@@ -38,10 +22,6 @@ class ListViewModel(private val repository: RoomRepository = Graph.repository) :
 
     var listDetailsState by mutableStateOf(ListDetailsState())
         private set
-
-
-//    private val _list = mutableStateOf()
-//    val list: MutableState<ArtworkList> = _list
 
     init {
         getLists()
@@ -62,7 +42,16 @@ class ListViewModel(private val repository: RoomRepository = Graph.repository) :
                 listDetailsState = listDetailsState.copy(list = listWithItems)
             }
         }
+    }
 
+    fun addToFavourites(artwork: Artwork) {
+        viewModelScope.launch {
+            val artworkItem = artwork.toArtworkItem(1L)
+            repository.insertArtworkItem(artworkItem)
+            repository.insertArtworkItemToList(
+                ArtListArtWorkCrossRef(artworkItem.artworkId, 1)
+            )
+        }
     }
 }
 data class ListState(
