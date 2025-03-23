a276a86b7ce848af6bdd84902eb5a59b563d90d5
Jackaria72 <jack.goodwill@hotmail.co.uk>
2025-03-23 15:35:07 +0000

    Feat: create Room Graph

---
+++ app/src/main/java/com/artful/curatolist/room/Graph.kt
@@ -0,0 +1,21 @@
+package com.artful.curatolist.room
+
+import android.content.Context
+import com.artful.curatolist.room.database.CuratolistDatabase
+import com.artful.curatolist.room.repository.RoomRepository
+
+object Graph {
+    lateinit var db:CuratolistDatabase
+        private set
+
+    val repository by lazy {
+        RoomRepository(
+            artworkListDao = db.artworkListDao(),
+            artworkItemDao = db.artworkItemDao()
+        )
+    }
+
+    fun provide(context:Context){
+        db = CuratolistDatabase.getDatabase(context)
+    }
+}
