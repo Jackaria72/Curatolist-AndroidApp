e9ad654b5b2bd8c9e0b43e96225c034fdb3c4b55
Jack <jack.goodwill@hotmail.co.uk>
2025-03-24 15:06:24 +0000

    Feature/styling (#20)
    * Feat: App styling
    
    * Feat: Add Acknowledgments
    
    * Feat: enable back button on details screen from lists
    
    * Feat: Home screen layout changes

--- app/src/main/java/com/artful/curatolist/ui/view/HomeScreen.kt
+++ app/src/main/java/com/artful/curatolist/ui/view/HomeScreen.kt
@@ -1,18 +1,33 @@
 package com.artful.curatolist.ui.view
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.displayCutoutPadding
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.artful.curatolist.R
 import com.artful.curatolist.ui.components.AppSearchBar
 import com.artful.curatolist.ui.navigation.NavDestination
 import com.artful.curatolist.viewmodel.ArtworkViewModel
+import java.nio.file.WatchEvent
 
 @Composable
 fun HomeScreen(navController: NavController, viewModel: ArtworkViewModel) {
@@ -28,5 +43,24 @@ fun HomeScreen(navController: NavController, viewModel: ArtworkViewModel) {
             false,
             {  }
         )
+        Spacer(modifier = Modifier.height(8.dp))
+        Image(
+            painter = painterResource(R.drawable.banner),
+            contentDescription = "Curatolist Banner"
+        )
+        Spacer(modifier = Modifier.height(10.dp))
+        Row(modifier = Modifier.fillMaxWidth().padding(10.dp)) {
+            Text(
+                text = """Hello and welcome to Curatolist! You're very own Art Gallery in your pocket!
+            |Browse Artworks from multiple museums, from the comfort of your own home or on the go! 
+            |Add them to your very own Exhibits in your own personal Gallery!
+        """.trimMargin(),
+                style = MaterialTheme.typography.bodyLarge
+            )
+
+        }
+        Spacer(modifier = Modifier.height(20.dp))
+
+        Text(text = "Go  forth and Curatolist!")
     }
 }
