7d73fd91c2934c7637de639f37a042b3906ed02a
Jackaria72 <jack.goodwill@hotmail.co.uk>
2025-03-24 00:01:55 +0000

    Feat: create ArtworkItem to Artwork converter

--- app/src/main/java/com/artful/curatolist/viewmodel/util/ArtworkConverter.kt
+++ app/src/main/java/com/artful/curatolist/viewmodel/util/ArtworkConverter.kt
@@ -20,3 +20,19 @@ fun Artwork.toArtworkItem(listId: Long): ArtworkItem {
         listId = listId
     )
 }
+fun ArtworkItem.toArtwork(): Artwork {
+    return Artwork(
+        id = this.artworkId,
+        title = this.title,
+        artist = this.artist,
+        date = this.date,
+        description = this.description,
+        medium = this.medium,
+        technique = this.technique,
+        classification = this.classification,
+        culturalOrigin = this.culturalOrigin,
+        dimensions = this.dimensions,
+        imageUrl = this.imageUrl,
+        source = this.source,
+    )
+}
