f41624fff646561c89a3a25b34bd40ccae8e9e61
Jackaria72 <jack.goodwill@hotmail.co.uk>
2025-03-24 05:37:40 +0000

    Feat: create setting button component

--- app/src/main/java/com/artful/curatolist/ui/components/ButtonsAndControls.kt
+++ app/src/main/java/com/artful/curatolist/ui/components/ButtonsAndControls.kt
@@ -1,15 +1,20 @@
 package com.artful.curatolist.ui.components
 
+import android.graphics.drawable.Icon
 import android.util.Log
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.automirrored.filled.ArrowForward
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
@@ -58,4 +63,21 @@ fun PreviewPaginationControls() {
         true,
         true
     )
+}
+@Composable
+fun SettingsButton(
+    onClick: () -> Unit,
+    text: String,
+    icon: ImageVector
+) {
+    Button(
+        onClick = onClick,
+    ) {
+        Text(text)
+        Spacer(modifier = Modifier.width(8.dp))
+        Icon(
+            icon,
+            contentDescription = text
+        )
+    }
 }
